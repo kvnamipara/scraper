e892701656db571937a4fe097cf694625d85cd8e
kvnamipara <kvnamipara@gmail.com>
2017-05-27 10:59:24 +0530

    beautiful soup intro

--- a.py
+++ a.py
@@ -1,4 +1,26 @@
 import requests
+from bs4 import BeautifulSoup
 
 page= requests.get("http://dataquestio.github.io/web-scraping-pages/simple.html")
-print page.status_code
+
+soup = BeautifulSoup(page.content,'html.parser')
+#print soup.prettify()
+a=list(soup.children)
+
+#print [type(item) for item in a]
+
+html= a[2]
+#print html
+html_children = list(html.children)
+#print html_children
+body = html_children[3]
+#print list(body.children)
+p= list(body.children)[1]
+#print p.get_text()
+
+
+##finding all intance at once
+print soup.find_all('p')
+
+##only first instance
+print soup.find('p')
--- s2.py
+++ s2.py
@@ -1,8 +1,11 @@
 import requests
-r= requests.get("https://api.github.com/events")
+#r= requests.get("https://api.github.com/events")
 #r = requests.post('http://httpbin.org/post', data = {'key':'value'})
-#payload = {'key1': 'value1', 'key2': 'value2'}
+payload = {'data': 'some'}
 #r = requests.get('http://httpbin.org/get', params=payload)
+url='https://api.github.com/some/endpoint'
+cookie= dict(cookies_are='working')
+r = requests.post(url, json=payload,cookies=cookie)
 
 
-print r.json()
+print r.text
